435660d9bfd8858bf9d09a9268ee3c60d54ea910
Andrew Avenoso <andrew.va@gmail.com>
2014-02-13 16:27:10 -0500

    books index uses model

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,8 +2,7 @@ class BooksController < ApplicationController
 
   def index
     @available_at = Time.now
-    @books = ["Atlas Shrugged", "Ender's Game",
-              "Hamlet", "The Hobbit"]
+    @books = Book.all
   end
 
 end
