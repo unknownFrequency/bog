dd2ef71b62d216a597109e1ac6f670776c4376d9
Scott Leberknight <scottleber@gmail.com>
2014-03-03 11:37:36 -0500

    Implement book controller test. Add id to 'All Books' link in book show page to make testing it easier.

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -1,14 +1,32 @@
 require 'test_helper'
 
 class BooksControllerTest < ActionController::TestCase
+  fixtures :books
+
   test "should get index" do
     get :index
     assert_response :success
+    assert_not_nil assigns(:books)
+
+    assert_select "h1", 1
+    assert_select "a", Book.count
   end
 
   test "should get show" do
-    get :show
+    hobbit_book = books(:hobbit)
+    get :show, id: hobbit_book.id
     assert_response :success
+
+    assert_select "header h2", "The Hobbit"
+
+    assert_select "p" do |elements|
+      html = elements.join(" ")
+      assert html.include?(hobbit_book.author), "should contain #{hobbit_book.author}"
+      assert html.include?(hobbit_book.pages.to_s), "should contain #{hobbit_book.pages}"
+      assert html.include?(hobbit_book.price.to_s), "should contain #{hobbit_book.price}"
+    end
+
+    assert_select "a#all_books_link", "All Books"
   end
 
 end
