172393c44a64b6bd5fc6ec2c90f57a2e091885df
Scott Leberknight <scottleber@gmail.com>
2014-03-06 17:25:42 -0500

    Put leading slash in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Books::Application.routes.draw do
 
   # get "/books" => "books#index", as: 'books'
-  # get "books/new" => "books#new", as: 'new_book'
+  # get "/books/new" => "books#new", as: 'new_book'
   # get "/books/:id" => "books#show", as: 'book'
-  # post "books" => "books#create"
+  # post "/books" => "books#create"
   # get "/books/:id/edit" => "books#edit", as: 'edit_book'
   # patch "/books/:id" => "books#update"
   # delete "/books/:id" => "books#destroy"
