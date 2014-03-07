1285fd12188ad52ad67b5c5307c7d4d26309d8ee
Scott Leberknight <scottleber@gmail.com>
2014-03-06 21:55:00 -0500

    Change second conditional validation to use an if, for sake of examples

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,8 +2,12 @@ class Book < ActiveRecord::Base
 
   validates :title, :author, :pages, :price, presence: true
 
-  validates :pages, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, unless: "pages.blank?"
+  validates :pages,
+            numericality: { only_integer: true, greater_than_or_equal_to: 0 },
+            unless: "pages.blank?"
 
-  validates :price, numericality: { greater_than_or_equal_to: 0 }, unless: "price.blank?"
+  validates :price,
+            numericality: { greater_than_or_equal_to: 0 },
+            if: "!price.blank?"
 
 end
