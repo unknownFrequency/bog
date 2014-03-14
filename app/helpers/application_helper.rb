3f10641d00765d46196f72ea21d2af58a6968e31
Andrew Avenoso <andrew.va@gmail.com>
2014-03-13 21:29:24 -0400

    layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,31 @@
 module ApplicationHelper
+  def flash_class(type)
+    case type
+    when :notice then 'alert-info'
+    when :alert then 'alert-warning'
+    else 'alert-info'
+    end
+  end
+
+  def bootstrap_form_field(f, field, &block)
+    div_style = ['form-group']
+    has_any_errors = f.object.errors.present? 
+    has_field_errors = f.object.errors[field].present?
+    div_style << (has_field_errors ? 'has-error' : 'has-success')
+    div_style << 'has-feedback' if has_any_errors
+    content_tag(:div, class:div_style.join(' ')) do
+      inner = f.label(field, class: 'col-sm-2 control-label')
+      inner += content_tag(:div, class:'col-sm-10') do
+        block.call if block
+      end
+      if has_any_errors
+        if has_field_errors
+          inner += content_tag(:i, nil, class:'glyphicon glyphicon-remove form-control-feedback')
+        else
+          inner += content_tag(:i, nil, class:'glyphicon glyphicon-ok form-control-feedback')
+        end
+      end
+      inner
+    end
+  end
 end
