f562caead6ac6cf2c947b36b6eae19ded6b3b1cd
Scott Leberknight <scottleber@gmail.com>
2014-03-17 17:05:13 -0400

    Add validation to Review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,12 @@
 class Review < ActiveRecord::Base
+
+  STARS = [1, 2, 3, 4, 5]
+
+  validates :name, :summary, :comment, presence: true
+
+  validates :stars, inclusion: { in: STARS, message: "must be between 1 and 5" }
+
   belongs_to :book
+
+
 end
