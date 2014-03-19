28cf590a27cd3d2779a4363f97d81fc993a2bb61
Scott Leberknight <scottleber@gmail.com>
2014-03-19 17:14:03 -0400

    Add format_average_stars helper method and use in the book show page.

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -7,4 +7,13 @@ module BooksHelper
     file_name = book.image_file_name.blank? ? 'placeholder.png' : book.image_file_name
     image_tag(file_name, class: 'img-rounded')
   end
+
+  def format_average_stars(book)
+    average = number_with_precision(book.average_stars, precision: 1)
+    if average
+      pluralize(average, 'star')
+    else
+      content_tag(:strong, 'No reviews')
+    end
+  end
 end
