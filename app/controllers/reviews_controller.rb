0ddff973208fb5ceb8851680458dce07f1569857
Scott Leberknight <scottleber@gmail.com>
2014-03-20 19:20:01 -0400

    Add ability to write new book reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,7 +6,24 @@ class ReviewsController < ApplicationController
     @reviews = @book.reviews.order('created_at desc')
   end
 
+  def new
+    @review = @book.reviews.new
+  end
+
+  def create
+    @review = @book.reviews.new(review_params)
+    if @book.save
+      redirect_to book_reviews_path(@book), notice: 'Thanks for your review!'
+    else
+      render :new
+    end
+  end
+
   private
+
+  def review_params
+    params.require(:review).permit(:name, :stars, :summary, :comment)
+  end
 
   def set_book
     @book = Book.find(params[:book_id])
