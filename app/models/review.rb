87549442c2e55a4e6d18002f798c2993a9c55612
Scott Leberknight <scottleber@gmail.com>
2014-03-20 18:38:51 -0400

    Add minimum length validation on Review.comment

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,9 +4,12 @@ class Review < ActiveRecord::Base
 
   validates :name, :summary, :comment, presence: true
 
+  validates :comment,
+            length: { minimum: 15, message: 'should have a bit more than that! (at least 15 characters)' },
+            unless: 'comment.blank?'
+
   validates :stars, inclusion: { in: STARS, message: "must be between 1 and 5" }
 
   belongs_to :book
-
 
 end
