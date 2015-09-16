e2546faf775ee4ab5a1ea4304a40695760105528
knogle <mac@macs-MacBook-Pro.local>
2015-09-16 11:31:43 +0200

    Devise installeret, og login link på forsiden som skifter til email når man er logget ind

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -3,10 +3,10 @@ module BooksHelper
     time.strftime('%B %d, %Y at %l:%M %p')
   end
 
-  def image_for(book)
-    file_name = book.image_file_name.blank? ? 'placeholder.png' : book.image_file_name
-    image_tag(file_name, class: 'img-rounded')
-  end
+  # def image_for(book)
+  #   file_name = book.image_file_name.blank? ? 'placeholder.png' : book.image_file_name
+  #   image_tag(file_name, class: 'img-rounded')
+  # end
 
   def format_average_stars(book)
     average = number_with_precision(book.average_stars, precision: 1)
