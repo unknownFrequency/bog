105db923ce9341d49b02ea65df954f24c2e047c5
unknownFrequency <rt@imusic.dk>
2016-09-19 20:21:22 +0200

    tilføjet medlem siden dato til profil side

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Books::Application.routes.draw do
-
   root 'books#index'
 
   resources :books do
@@ -8,16 +7,16 @@ Books::Application.routes.draw do
       get 'bargains'
       get 'bargains/page/:page', :action => :bargains
     end
-  end
 
-    #resources :users, only: [:show]
     resources :reviews
     resources :messages
-    resources :photos
-    resources :messages
+  end
 
-    devise_for :users, :controllers => { registrations: 'registrations' }
-    get 'users',          to: 'users#index',   as: :all_users
-    get 'users/:id',      to: 'users#show',    as: :single_user
-    get 'users/:id/edit', to: 'users#edit',    as: :edit_user
+
+  resources :messages
+  resources :photos
+  devise_for :users
+  get 'users',          to: 'users#index',   as: :all_users
+  get 'users/:id',      to: 'users#show',    as: :single_user
+  get 'users/:id/edit', to: 'users#edit',    as: :edit_user
 end
