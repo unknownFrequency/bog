cc1b0ca517600d962ffa070daf46b9db07600510
unknownFrequency <rt@imusic.dk>
2016-09-19 20:51:52 +0200

    Viser bøger som profilen har til salg

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @books = Book.find(:all, :conditions => {:user_id => params['user_id']}) 
   end
 
 
