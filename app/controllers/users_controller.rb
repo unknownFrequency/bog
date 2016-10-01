66a45f97c394481c64345a7c858bafa385d7687f
Your Name <you@example.com>
2016-10-01 22:43:25 +0200

    Tilføjet stjerne billeder til vurdering af en bog

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,11 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @books = Book.find(:all, :conditions => {:user_id => params['user_id']}) 
+    @books = Book.find(:all, :conditions => {:user_id => params['id']}) 
   end
 
+
+
+  
 
 end
--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -16,4 +16,19 @@ module BooksHelper
       content_tag(:span, 'Ingen anmeldelser', { class: 'reviews' })
     end
   end
+
+  def star_pics(avarage)
+    if avarage.to_int < 1.5
+      content_tag(:span, image_tag('1_star.png', size: "100x30"))
+    elsif avarage.to_int > 1.5 && avarage.to_int < 2.5
+      content_tag(:span, image_tag('2_star.png', size: "100x30"))
+    elsif avarage.to_int > 2.5 && avarage.to_int < 3.5
+      content_tag(:span, image_tag('3_star.png', size: "100x30"))
+    elsif avarage.to_int > 3.5 && avarage.to_int < 4.5
+      content_tag(:span, image_tag('4_star.png', size: "100x30"))
+    elsif avarage.to_int > 4.5 
+      content_tag(:span, image_tag('5_star.png', size: "100x30"))
+    end
+  end
+
 end
