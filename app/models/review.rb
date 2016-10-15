0f5ac0361f83834327aac5ca2f4cb7966c63122c
unknownFrequency <rt@imusic.dk>
2016-10-15 14:23:45 +0200

    Lidt brugerkontrol på review og kontakt sælger

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,14 +2,14 @@ class Review < ActiveRecord::Base
 
   STARS = 1..5
 
-  validates :summary, :comment, presence: true
+  validates :comment, presence: true
 
   validates :comment,
             length: { minimum: 10, message: 'Mindst 10 tegn, tak.' },
             unless: 'comment.blank?'
 
-  #validates :stars, inclusion: { in: STARS,
-  #  message: "must be between #{STARS.first} and #{STARS.last}" }
+  validates :stars, inclusion: { in: STARS,
+    message: "Skal være mellem #{STARS.first} og #{STARS.last}" }
 
   belongs_to :book
 
